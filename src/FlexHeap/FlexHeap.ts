85d34ecd286eb50ec61f8ff6938ef192deafc653
Wes Harper <wes.harper@ibotta.com>
2020-02-23 19:28:38 -0700

    implement BFS algorithms and add custom helper classes

---
+++ src/FlexHeap/FlexHeap.ts
@@ -0,0 +1,82 @@
+export default class FlexHeap<T> {
+  items: (T | null)[] = [null];
+  comparator: (oldEl: T, newEl: T) => boolean;
+
+  // using a comparator to allow the implementer to choose their own heap type
+  // comparator also allows for comparing complex objects
+  constructor(comparator: (oldEl: T, newEl: T) => boolean) {
+    this.comparator = comparator;
+  }
+
+  get size(): number {
+    // the null value at the beginning of the array should be considered immutable
+    // this hides the implementation details from the user and allows for easy checking
+    return this.items.length - 1;
+  }
+
+  insert(val: T): void {
+    this.items.push(val);
+    let currentIndex = this.size;
+
+    while (true) {
+      const parentIndex = Math.floor(currentIndex / 2);
+      if (parentIndex < 1) {
+        break;
+      }
+      if (
+        this.comparator(this.items[parentIndex]!, this.items[currentIndex]!)
+      ) {
+        [this.items[parentIndex], this.items[currentIndex]] = [
+          this.items[currentIndex],
+          this.items[parentIndex]
+        ];
+        currentIndex = parentIndex;
+      } else {
+        break;
+      }
+    }
+  }
+
+  extract(): T | null {
+    if (this.size < 1) {
+      return null;
+    }
+
+    let currentIndex = 1;
+    // swap removal value and last value
+    [this.items[this.size], this.items[currentIndex]] = [
+      this.items[currentIndex],
+      this.items[this.size]
+    ];
+    const resultVal = this.items.pop()!;
+    while (true) {
+      const firstChildIndex = currentIndex * 2;
+      const secondChildIndex = firstChildIndex + 1;
+      if (
+        firstChildIndex <= this.size &&
+        this.comparator(this.items[currentIndex]!, this.items[firstChildIndex]!)
+      ) {
+        [this.items[firstChildIndex], this.items[currentIndex]] = [
+          this.items[currentIndex],
+          this.items[firstChildIndex]
+        ];
+        currentIndex = firstChildIndex;
+      } else if (
+        secondChildIndex <= this.size &&
+        this.comparator(
+          this.items[currentIndex]!,
+          this.items[secondChildIndex]!
+        )
+      ) {
+        [this.items[secondChildIndex], this.items[currentIndex]] = [
+          this.items[currentIndex],
+          this.items[secondChildIndex]
+        ];
+        currentIndex = secondChildIndex;
+      } else {
+        break;
+      }
+    }
+    return resultVal;
+  }
+}
--- src/Graph/Graph.ts
+++ src/Graph/Graph.ts
@@ -1,6 +1,12 @@
 import Edge, { EdgeWeight } from './Edge';
 import Vertex from './Vertex';
 import Stack from '@/Stack/Stack';
+import Queue from '@/Queue/Queue';
+import FlexHeap from '@/FlexHeap/FlexHeap';
+
+// combine vertex with path information for use in pathfinding algorithms
+// include EdgeWeight as an optional third value for best path algorithms
+type VertexWithPath<T> = [Vertex<T>, number[], EdgeWeight?];
 
 export default class Graph<T> {
   // must set directed property at instantiation
@@ -45,9 +51,7 @@ export default class Graph<T> {
     weight = weight || 1;
     const fromVertex = this.getVertex(fromId);
     const toVertex = this.getVertex(toId);
-    if (!fromVertex || !toVertex) {
-      throw new Error('VertexError: vertex does not exist');
-    }
+    this._passOrThrowVertexException(fromVertex, toVertex);
     // provide both `to` and `from` weights if graph is directed
     // otherwise, leave unset weight `null`
     const toEdge = this.directed ? new Edge(weight) : new Edge(weight, weight);
@@ -56,8 +60,8 @@ export default class Graph<T> {
       : new Edge(weight, weight);
 
     // set edge in both adjacency maps for faster lookup and vertex removal
-    fromVertex.edges.set(toId, toEdge);
-    toVertex.edges.set(fromId, fromEdge);
+    fromVertex!.edges.set(toId, toEdge);
+    toVertex!.edges.set(fromId, fromEdge);
   }
 
   // this removes an edge in one direction if directed
@@ -82,20 +86,16 @@ export default class Graph<T> {
 
   setVertex(id: number, val: T): void {
     const vertex = this.getVertex(id);
-    if (!vertex) {
-      throw new Error('VertexError: vertex does not exist');
-    }
-    vertex.val = val;
+    this._passOrThrowVertexException(vertex);
+    vertex!.val = val;
   }
 
   // returns entire edge for one vertex including both to and from weights
   getEdge(fromId: number, toId: number): Edge | undefined {
     const fromVertex = this.getVertex(fromId);
     const toVertex = this.getVertex(toId);
-    if (!fromVertex || !toVertex) {
-      throw new Error('VertexError: vertex does not exist');
-    }
-    return fromVertex.edges.get(toId);
+    this._passOrThrowVertexException(fromVertex, toVertex);
+    return fromVertex!.edges.get(toId);
   }
 
   // set edge to desired weight
@@ -122,30 +122,28 @@ export default class Graph<T> {
   isReachable(startingVertexId: number, endingVertexId: number): number[] {
     const startVertex = this.getVertex(startingVertexId);
     const endVertex = this.getVertex(endingVertexId);
-    if (!startVertex || !endVertex) {
-      throw new Error('VertexError: vertex does not exist');
-    }
+    this._passOrThrowVertexException(startVertex, endVertex);
     // this marks vertices as "visited"
     const visitedVertices = new Set<Vertex<T>>();
     // this will keep track of our vertices in a DFS manner as we go
-    const pathStack = new Stack<[Vertex<T>, number[]]>();
+    const pathStack = new Stack<VertexWithPath<T>>();
 
-    visitedVertices.add(startVertex);
-    pathStack.push([startVertex, [startVertex.id]]);
+    pathStack.push([startVertex!, [startVertex!.id]]);
     while (pathStack.head) {
       // get vertex off the top of the stack (furthest along in the path DFS)
       const stackNode = pathStack.pop()!;
       const currentVertex = stackNode[0];
       const currentPath = stackNode[1];
+
+      visitedVertices.add(currentVertex);
       // if the current vertex is our destination vertex, break the loop
       if (currentVertex.id === endingVertexId) {
         return currentPath;
       }
-      currentVertex.edges.forEach((_, vertexId: number) => {
+      currentVertex.edges.forEach((edge: Edge, vertexId: number) => {
         const vertex = this.getVertex(vertexId)!;
         // add each vertex to the stack if it hasn't already been visited
-        if (!visitedVertices.has(vertex)) {
-          visitedVertices.add(vertex);
+        if (edge.to !== null && !visitedVertices.has(vertex)) {
           pathStack.push([vertex, [...currentPath, vertex.id]]);
         }
       });
@@ -157,32 +155,30 @@ export default class Graph<T> {
   allPaths(startingVertexId: number, endingVertexId: number): number[][] {
     const startVertex = this.getVertex(startingVertexId);
     const endVertex = this.getVertex(endingVertexId);
-    if (!startVertex || !endVertex) {
-      throw new Error('VertexError: vertex does not exist');
-    }
+    this._passOrThrowVertexException(startVertex, endVertex);
     // store all valid paths
     const result: number[][] = [];
     // this marks vertices as "visited"
     const visitedVertices = new Set<Vertex<T>>();
     // this will keep track of our vertices in a DFS manner as we go
-    const pathStack = new Stack<[Vertex<T>, number[]]>();
+    const pathStack = new Stack<VertexWithPath<T>>();
 
-    visitedVertices.add(startVertex);
-    pathStack.push([startVertex, [startVertex.id]]);
+    pathStack.push([startVertex!, [startVertex!.id]]);
     while (pathStack.head) {
       // get vertex off the top of the stack (furthest along in the path DFS)
       const stackNode = pathStack.pop()!;
       const currentVertex = stackNode[0];
       const currentPath = stackNode[1];
+
+      visitedVertices.add(currentVertex);
       // if the current vertex is our destination vertex, add path to list of result paths
       if (currentVertex.id === endingVertexId) {
         result.push(currentPath);
       }
-      currentVertex.edges.forEach((_, vertexId: number) => {
+      currentVertex.edges.forEach((edge: Edge, vertexId: number) => {
         const vertex = this.getVertex(vertexId)!;
         // add each vertex to the stack if it hasn't already been visited
-        if (!visitedVertices.has(vertex)) {
-          visitedVertices.add(vertex);
+        if (edge.to !== null && !visitedVertices.has(vertex)) {
           pathStack.push([vertex, [...currentPath, vertex.id]]);
         }
       });
@@ -193,12 +189,87 @@ export default class Graph<T> {
   // return a path representing the fewest possible "turns"
   // essentially the shortest path ignoring edge weight
   // returns an array of vertex IDs
-  // shortestPath(startingVertexId: number, endingVertexId: number): number[] {}
+  shortestPath(startingVertexId: number, endingVertexId: number): number[] {
+    const startVertex = this.getVertex(startingVertexId);
+    const endVertex = this.getVertex(endingVertexId);
+    this._passOrThrowVertexException(startVertex, endVertex);
+
+    const visitedVertices = new Set<number>();
+    const pathQueue = new Queue<VertexWithPath<T>>();
+
+    pathQueue.enqueue([startVertex!, [startVertex!.id]]);
+
+    while (pathQueue.head) {
+      const queueNode = pathQueue.dequeue()!;
+      const currentVertex = queueNode[0];
+      const currentPath = queueNode[1];
+
+      if (currentVertex.id === endingVertexId) {
+        return currentPath;
+      }
+
+      visitedVertices.add(currentVertex.id);
+      currentVertex.edges.forEach((edge: Edge, vertexId: number): void => {
+        if (edge.to !== null && !visitedVertices.has(vertexId)) {
+          const vertex = this.getVertex(vertexId)!;
+          pathQueue.enqueue([vertex, [...currentPath, vertex.id]]);
+        }
+      });
+    }
+    return [];
+  }
 
   // return a path representing the fastest possible route between vertices
   // this accounts for edge weight
   // returns an array of vertex IDs
-  // quickestPath(startingVertexId: number, endingVertexId: number): number[] {}
+  // by using a comparator, this method can also be used as an a* implementation of Best First Search
+  // this also allows the implementer to choose whether a higher or lower edge weight should be considered "best"
+  quickestPath(
+    startingVertexId: number,
+    endingVertexId: number,
+    comparator?: (
+      visited: VertexWithPath<T>,
+      current: VertexWithPath<T>
+    ) => boolean
+  ): number[] {
+    const startVertex = this.getVertex(startingVertexId);
+    const endVertex = this.getVertex(endingVertexId);
+    this._passOrThrowVertexException(startVertex, endVertex);
+
+    // prioritize smallest edge weight by default
+    comparator =
+      comparator ||
+      ((visited: VertexWithPath<T>, current: VertexWithPath<T>): boolean =>
+        visited[2]! > current[2]!);
+
+    const visitedVertices = new Set<number>();
+    // use a priority queue to choose the next best path
+    const pathQueue = new FlexHeap<VertexWithPath<T>>(comparator);
+    pathQueue.insert([startVertex!, [startVertex!.id], 0]);
+    while (pathQueue.size > 0) {
+      const vertexWithPath = pathQueue.extract()!;
+      const currentVertex = vertexWithPath[0];
+      const currentPath = vertexWithPath[1];
+      const currentEdgeWeight = vertexWithPath[2]!;
+
+      if (currentVertex.id === endingVertexId) {
+        return currentPath;
+      }
+
+      visitedVertices.add(currentVertex.id);
+
+      currentVertex.edges.forEach((edge: Edge, vertexId: number): void => {
+        if (edge.to !== null && !visitedVertices.has(vertexId)) {
+          const vertex = this.getVertex(vertexId)!;
+          pathQueue.insert([
+            vertex,
+            [...currentPath, vertex.id, currentEdgeWeight + edge.to]
+          ]);
+        }
+      });
+    }
+    return [];
+  }
 
   // remove an edge from all adjacency lists entirely
   private _deleteEdge(fromId: number, toId: number): void {
@@ -206,5 +277,15 @@ export default class Graph<T> {
     const toVertex = this.getVertex(toId);
     fromVertex!.edges.delete(toId);
     toVertex!.edges.delete(fromId);
+  }
+
+  private _passOrThrowVertexException(
+    ...vertices: (Vertex<T> | undefined)[]
+  ): void | never {
+    vertices.forEach((vertex: Vertex<T> | undefined): void | never => {
+      if (!vertex) {
+        throw new Error('VertexError: vertex does not exist');
+      }
+    });
   }
 }
