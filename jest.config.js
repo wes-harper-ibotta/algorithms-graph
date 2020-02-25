b3259130222ce6a13c1d5991a13e31227f016329
Wes Harper <wes.harper@ibotta.com>
2020-02-24 18:52:08 -0700

    add tests for enqueue()

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  setupFilesAfterEnv: ['./jest.setup.js']
+  preset: 'ts-jest',
+  testEnvironment: 'node'
 };
--- tests/Queue/Queue.spec.ts
+++ tests/Queue/Queue.spec.ts
@@ -1,3 +1,45 @@
 import { Queue } from '../../src/Queue';
 
-describe('`Queue`', () => {});
+describe('`Queue`', () => {
+  describe('`enqueue()`', () => {
+    describe('when queue is empty', () => {
+      test('new node becomes both `head` and `tail` of queue', () => {
+        const q = new Queue<number>();
+        q.enqueue(1);
+        expect(q.head!.val).toBe(1);
+        expect(q.tail!.val).toBe(1);
+      });
+    });
+
+    describe('when queue has at least one value', () => {
+      let q: Queue<number>;
+      beforeEach(() => {
+        q = new Queue<number>();
+        q.enqueue(1);
+      });
+
+      test('new node becomes new `tail` of queue', () => {
+        q.enqueue(2);
+        expect(q.tail!.val).toBe(2);
+      });
+
+      test('`head` remains unchanged', () => {
+        q.enqueue(2);
+        expect(q.head!.val).toBe(1);
+      });
+
+      test('queue retains insertion order', () => {
+        q.enqueue(2);
+        q.enqueue(3);
+        q.enqueue(4);
+
+        let currNode = q.head;
+        let testNum = 1;
+        while (currNode) {
+          expect(currNode!.val).toBe(testNum++);
+          currNode = currNode.next;
+        }
+      });
+    });
+  });
+});
